9542a193a835679cb6259fe5c47806e2d409cece
Sumitsh28 <sumit.off28@gmail.com>
2024-07-07 18:15:25 +0530

    Analytics Added

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
